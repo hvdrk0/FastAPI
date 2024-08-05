28e56580939c705df176b54e48471f1fa2ee42cc
hvdrk0 <hvdrk0@gmail.com>
2024-08-05 19:13:26 +0900

    d

---
+++ 2/model.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+class ITEM(BaseModel):
+    item:str
+    status:str
+
+class Todo(BaseModel):
+    id:int
+    item:str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id":1,
+                "item":"Example Schema!"
+            }
+        }
+
+
+    
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example":{
+                "item": "Read the next chapter of the book"
+            }
+        }
